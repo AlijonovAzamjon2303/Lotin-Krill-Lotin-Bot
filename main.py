63f218f1ae04376f397060a7baa2a337cca5924f
Abdurashidova23 <marjonafozilovna@gmail.com>
2024-10-28 09:23:35 +0500

    Telebot moduli va TOKEN qo'shildi

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from transliterate import to_cyrillic, to_latin
+import telebot
+
+TOKEN = '6542995913:AAHoafmHxnCXrqo1K8nmOgouuMdozlq6XNE'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 matn = input("Matn kiriting : ")
 
